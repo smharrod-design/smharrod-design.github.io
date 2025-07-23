b6b32a4ec95b6bb7639bbdc8d7c73541d17bd596
smharrod-design <smharrod@outlook.com>
2025-07-23 11:44:42 +0100

    swiper update

--- assets/swiper-init.js
+++ assets/swiper-init.js
@@ -4,12 +4,16 @@ const swiper = new Swiper('.swiper', {
     delay: 3000,
     disableOnInteraction: false,
   },
+    effect: 'fade', 
+  fadeEffect: {
+    crossFade: true,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   pagination: {
     el: '.swiper-pagination',
-    type: 'fraction', // shows 1 / 50 instead of dots
+    type: 'fraction', 
   },
 });
